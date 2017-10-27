f05976845ccb4d6b2af658070db02388a4e8b4e1
Philip Hoang <philip.t.hoang@outlook.com>
2017-10-27 14:55:29 +0200

    Tweaks round 2

--- INF100/Exercises/Assignment5/AssignStudents.java
+++ INF100/Exercises/Assignment5/AssignStudents.java
@@ -7,10 +7,10 @@ import java.io.FileWriter;
 public class AssignStudents{
 
   /**
-   * Reads each line from a file and puts it in an ArrayList of strings
-   * @param  String filename      The textfile
-   * @return an ArrayList from the file
-   */
+  * Reads each line from a file and puts it in an ArrayList of strings
+  * @param  String filename      The textfile
+  * @return an ArrayList from the file
+  */
   public static ArrayList<String> namesFromFilename(String filename) {
 
     ArrayList<String> nameList = new ArrayList<>();
@@ -28,28 +28,37 @@ public class AssignStudents{
     catch(FileNotFoundException e) {
       System.out.println("File not found with exception: " + e);
     }
+    catch(IOException e) {
+      System.out.println("Wrong with reading the file: " + e);
+    }
 
     return nameList;
   }
 
   /**
-   * Assign the list of students to each graders
-   * @param  ArrayList<String> students      list of students
-   * @param  ArrayList<String> graders       list of graders
-   * @return a combined list where students are assigned to each graders
-   */
+  * Assign the list of students to each graders
+  * @param  ArrayList<String> students      list of students
+  * @param  ArrayList<String> graders       list of graders
+  * @return a combined list where students are assigned to each graders
+  */
   public static ArrayList<String> assignGraders(ArrayList<String> students, ArrayList<String> graders) {
     ArrayList<String> gradersWithStudents = new ArrayList<>();
     int studentSize = students.size();
     int graderSize = graders.size();
+    int[] studentsPerGrader = new int[graderSize];
 
     int count = 0;
-    for(String s : students) {
-      gradersWithStudents.add(s + ", " + graders.get(count));
-      count++;
-      if(count == graderSize) {
-        count = 0;
-      }
+    for (int i = 0; i < studentSize; i++) {
+        gradersWithStudents.add(students.get(i) + ", " +  graders.get(count));
+          studentsPerGrader[count++]++;
+          if (count == graderSize) {
+            count = 0;
+          }
+    }
+
+    for(int j = 0; j < studentsPerGrader.length; j++) {
+      System.out.println("Assigned " + studentsPerGrader[j] + " students to " +
+      graders.get(j));
     }
 
     return gradersWithStudents;
@@ -57,10 +66,10 @@ public class AssignStudents{
 
 
   /**
-   * Takes a list and writes it to a textfile
-   * @param ArrayList<String> assignment an ArrayList
-   * @param String            filename   name of the textfile
-   */
+  * Takes a list and writes it to a textfile
+  * @param ArrayList<String> assignment an ArrayList
+  * @param String            filename   name of the textfile
+  */
   public static void writeAssignment(ArrayList<String> assignment, String filename) {
     try {
       FileWriter writer = new FileWriter(filename + ".txt");
